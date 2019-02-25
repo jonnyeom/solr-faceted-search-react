b57297dd5975951378e6f2acedb46ef6ca42754d
Jes Constantine <jesconstantine@gmail.com>
2019-02-25 09:10:29 -0500

    root-138: don't set suggest query param if no suggestQueryField value

--- src/api/solr-query.js
+++ src/api/solr-query.js
@@ -194,8 +194,10 @@ const buildSuggestQuery = (fields, suggestQueryField) => {
     // additional characters at the end of the users search term.
     // @see: https://lucene.apache.org/solr/guide/6_6/the-standard-query-parser.html#TheStandardQueryParser-WildcardSearches
     // @see: https://opensourceconnections.com/blog/2013/06/07/search-as-you-type-with-solr/
-    // We also set the default field to the mainQueryField.
-    return `${searchField.value}+${searchField.value}*&df=${searchField.field}`;
+    // We also set the default field to the suggestQueryField.
+    return searchField.value !== ""
+      ? `${searchField.value}+${searchField.value}*&df=${searchField.field}`
+      : "";
   });
   // If there are multiple suggest query fields, join them.
   if (params.length > 1) {
